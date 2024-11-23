d2c6e546b8ed53d3a6dbfc87651939ffedf1331d
Mohammed yaser <100796943+Mhdyaser123@users.noreply.github.com>
2024-11-24 04:33:50 +0530

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : calc.c
+ Author      : Mohammed Yaser
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float num1,num2,result;
+	int option;
+	setbuf(stdout,NULL);
+	printf("A SIMPLE CALCULATOR\n");
+	printf("-created by Yaser\n");
+	printf("Enter 2 Numbers");
+	scanf("%f%f",&num1,&num2);
+
+	printf("please select any option from below\n");
+
+	printf("1 for addition\n2 for subtraction\n3 for multiplication\n"
+			"4 for division");
+	scanf("%d",&option);
+
+	if(option==1){
+		result=num1+num2;
+		printf("%f",result);
+	}else if(option==2){
+		result=num1-num2;
+		printf("%f",result);
+	}if(option==3){
+		result=num1*num2;
+		printf("%f",result);
+	}if(option==4){
+		result=num1/num2;
+		printf("%f",result);
+	}
+
+	return EXIT_SUCCESS;
+}
